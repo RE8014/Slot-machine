6a2d183eead611ac5ce1dc1aa36fc9748288dc83
RE8014 <karlsiu1001@gmail.com>
2023-05-20 13:45:53 +0800

    加入專案檔案。

---
+++ Slot machine/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Slot_machine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.money = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Azonix", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(387, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "7";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(179)))), ((int)(((byte)(1)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(161)))), ((int)(((byte)(1)))));
+            this.button1.FlatAppearance.BorderSize = 4;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(194)))), ((int)(((byte)(52)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(179)))), ((int)(((byte)(1)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Azonix", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(287, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 55);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Azonix", 47.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(449, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 64);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "7";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Azonix", 47.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(348, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 64);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "7";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Azonix", 47.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(247, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 64);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "7";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Slot_machine.Properties.Resources.圖層_11;
+            this.pictureBox2.Location = new System.Drawing.Point(179, 92);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(443, 232);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.money);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(172, 56);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Slot_machine.Properties.Resources.star;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // money
+            // 
+            this.money.AutoSize = true;
+            this.money.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.money.Font = new System.Drawing.Font("Azonix", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.money.Location = new System.Drawing.Point(59, 0);
+            this.money.Name = "money";
+            this.money.Size = new System.Drawing.Size(28, 56);
+            this.money.TabIndex = 1;
+            this.money.Text = "0";
+            this.money.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Slot_machine.Properties.Resources.圖層_41;
+            this.pictureBox3.Location = new System.Drawing.Point(179, 92);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(443, 232);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slot machine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label money;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Slot machine/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot_machine
+{
+    public partial class Form1 : Form
+    {
+        private int coin =  0;
+        private int result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Visible = false;
+            button1.Visible = false;
+            label2.ForeColor = Color.Gray;
+            label3.ForeColor = Color.Gray;
+            label4.ForeColor = Color.Gray;
+            Random random1 = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                int first = random1.Next(1, 8);
+                await Task.Delay(100);
+                label2.Text = first.ToString();
+            }
+            label2.ForeColor = Color.Black;
+            Random random2 = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                int second = random2.Next(1, 8);
+                await Task.Delay(100);
+                label3.Text = second.ToString();
+
+            }
+            label3.ForeColor = Color.Black;
+            Random random3 = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                int third = random3.Next(1, 8);
+                await Task.Delay(100);
+                label4.Text = third.ToString();
+
+            }
+            label4.ForeColor = Color.Black;
+            result = int.Parse(label2.Text + label3.Text + label4.Text);
+            int slot1 = int.Parse(label2.Text);
+            int slot2 = int.Parse(label3.Text);
+            int slot3 = int.Parse(label4.Text);
+            label5.Text = result.ToString();
+            if (result == 777)
+            {
+                coin += 100;
+            }
+            else if (slot1 == slot2 && slot2 == slot3)
+            {
+                coin += 50;
+            }
+            else if (slot1 == slot2 || slot1 == slot3 || slot2 == slot3)
+            {
+                coin += 10;
+            }
+            else
+            {
+                coin += 0;
+            }
+            if (slot1%2 == 0 && slot2 % 2 == 0 && slot3 % 2 == 0)
+            {
+                coin += 5;
+            }
+            money.Text = coin.ToString();
+            button1.Visible = true;
+        }
+
+        private async void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            await Task.Delay(200);
+            pictureBox2.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            money.Text = coin.ToString();
+        }
+    }
+}
